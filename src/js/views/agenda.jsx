ff9d8c6d502985c8e209c55ea91048cd0a776e81
Miguel <Megl290304@gmail.com>
2024-01-29 15:21:42 -0400

    commit

--- src/js/views/agenda.jsx
+++ src/js/views/agenda.jsx
@@ -1,88 +1,90 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
-const EditContact = () => {
-  const { actions } = useContext(Context);
-
-  const navigate = useNavigate();
+const Agenda = () => {
+  const navigate = useNavigate()
   const navigateToHome = () => {
-    navigate("/")
+   navigate("/")
   }
 
-  const params = useParams();
-  console.log(params);
+  const { actions } = useContext(Context);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    actions.editContact({
+    actions.getNewContact({
       full_name: e.target.name.value,
       email: e.target.email.value,
       agenda_slug: "agenda",
       address: e.target.location.value,
       phone: e.target.phone.value,
-    }, params.id);
+    });
     navigateToHome();
   };
 
   return (
-    <form 
+    <div>
+    <form
       className="container mt-4 p-4 bg-dark text-light rounded"
       onSubmit={handleSubmit}
     >
-      <h1>Edit Contact</h1>
+      <h1>New Contact</h1>
       <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">
-          Change Name
+        <label className="form-label">
+          Name
         </label>
         <input
           name="name"
           type="text"
           className="form-control"
+          placeholder="of the next..."
         />
       </div>
-      
+
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Change Location
+        <label className="form-label">
+          Location
         </label>
         <input
           name="location"
           type="text"
           className="form-control"
+          placeholder="Where the victim lives?"
         />
       </div>
 
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Change Phone
+        <label className="form-label">
+          Phone
         </label>
         <input
           name="phone"
           type="tel"
           className="form-control"
+          placeholder="Phone number of our dear friend?"
         />
       </div>
 
-      
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Change Email
+        <label className="form-label">
+          Email
         </label>
         <input
           name="email"
           type="email"
           className="form-control"
+          placeholder="We have your email"
         />
       </div>
 
       <div className="d-flex justify-content-end">
         <button type="submit" className="btn btn-primary">
-          Save Changes
+          Save
         </button>
       </div>
     </form>
+    </div>
   );
 };
 
-export default EditContact;
+export default Agenda;
